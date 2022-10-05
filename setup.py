cfa6e063ae40b1eef997163089d1a92babeff641
AzureAD\DanielKoops <daniel.koops@itility.nl>
2022-10-05 14:45:39 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="EasyAdls",
+    version="0.1.0",
+    author="D. Koops",
+    description="Wrapper around the Azure Storage Blobs SDK to make life a bit easier",
+    url='https://github.com/danielkoops1/easy_adls',
+    install_requires=[
+        'pandas>=1.2.4',
+        'azure-storage-blob>=12.13.1',
+          ],
+)
